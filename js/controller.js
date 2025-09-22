a7756cb2cf7904c4c0ab38720acdb1b02129dd62
MatheuxP <matheuspupiaa@gmail.com>
2025-09-22 10:29:37 -0300

    logo component script

--- js/controller.js
+++ js/controller.js
@@ -1,7 +1,9 @@
 {/* 
     <div id="header-component"></div>
 
-    <div id="footer-component"></div> 
+    <div id="footer-component"></div>
+    
+    <div id="logo-component"></div>
 */}
 
 
@@ -10,22 +12,38 @@ document.addEventListener("DOMContentLoaded", function () {
     const headerPlaceholder = document.getElementById('header-component');
     const footerPlaceholder = document.getElementById('footer-component');
 
+
     fetch('../components/header.html')
         .then(response => response.text())
         .then(data => {
             headerPlaceholder.innerHTML = data;
+
+    
+            const logoPlaceholder = document.getElementById('logo-component');
+            if (logoPlaceholder) {
+                fetch('../components/logo.html')
+                    .then(response => response.text())
+                    .then(logoData => {
+                        logoPlaceholder.innerHTML = logoData;
+                    })
+                    .catch(error => {
+                        console.error('Erro ao carregar a logo:', error);
+                        logoPlaceholder.innerHTML = "<p>Erro ao carregar a logo.</p>";
+                    });
+            }
         })
         .catch(error => {
             console.error('Erro ao carregar o header:', error);
             headerPlaceholder.innerHTML = "<p>Erro ao carregar o header.</p>";
         });
+ 
     fetch('../components/footer.html')
         .then(response => response.text())
         .then(data => {
             footerPlaceholder.innerHTML = data;
         })
         .catch(error => {
-            console.error('Erro ao carregar o header:', error);
-            footerPlaceholder.innerHTML = "<p>Erro ao carregar o header.</p>";
+            console.error('Erro ao carregar o footer:', error);
+            footerPlaceholder.innerHTML = "<p>Erro ao carregar o footer.</p>";
         });
 });
